3d7d4d8102e853ba4b2868cd1f771a86dcf3d495
studiousTribe <studioustribe@gmail.com>
2021-06-21 17:34:51 +0530

    removed debugger from JS file

--- script.js
+++ script.js
@@ -66,7 +66,6 @@ async function getPhotos() {
     photosArray = await response.json();
     displayPhotos();
     if (initialLoad) {
-      debugger;
       initialLoad = false;
       callAPIWithNewCount(25);
     }
